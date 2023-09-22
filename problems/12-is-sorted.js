89efc026ce21fc69a4665773d39404bb6a3c02b2
Thomas Vogel <Lenex5109@yahoo.com>
2023-09-21 22:54:13 -0500

    done

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -11,10 +11,22 @@ isSorted([5, 4, 3, 2, 1]); // false
 ***********************************************************************/
 
 
-function isSorted(arr) {
-  // Your code here 
-}
+const isSorted = arr => {
+  const checkSorted = (array, index) => {
+      if (index === array.length - 1) return true;
 
+      if (array[index] > array[index + 1]) return false;
+
+      return checkSorted(array, index + 1);
+  };
+
+  return checkSorted(arr, 0);
+};
+
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+console.log(isSorted([1, 2, 4, 3, 5])); // false
+console.log(isSorted([2, 4, 6, 7, 8])); // true
+console.log(isSorted([5, 4, 3, 2, 1])); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
